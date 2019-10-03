4e7bb2c3e571d006442fa88ef2911265e3e16444
ivan <>
2019-10-02 22:41:50 -0300

    Tp 2 Terminado

--- tp_laboratorio_2/utn.h
+++ tp_laboratorio_2/utn.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef UTN_H_
 #define UTN_H_
 int getString(	char *pResultado,
@@ -27,33 +26,3 @@ int getChar(char *resultado,
 			int reintentos);
 
 #endif /* UTN_H_ */
-=======
-#ifndef UTN_H_
-#define UTN_H_
-int getString(	char *pResultado,
-				char *pMensaje,
-				char *pMensajeError,
-				int minimo,
-				int maximo,
-				int reintentos);
-int getInt(	int *pResultado,
-			char *pMensaje,
-			char *pMensajeError,
-			int minimo,
-			int maximo,
-			int reintentos);
-int getFloat(	float *resultado,
-				char *mensaje,
-				char *mensajeError,
-				float minimo,
-				float maximo,
-				int reintentos);
-int getChar(char *resultado,
-			char *mensaje,
-			char *mensajeError,
-			int minimo,
-			int maximo,
-			int reintentos);
-
-#endif /* UTN_H_ */
->>>>>>> b55dfb6eb7752af3b8f2774976e94f3539e20d6b
